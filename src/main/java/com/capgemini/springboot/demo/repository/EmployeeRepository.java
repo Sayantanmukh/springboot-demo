5b304f82013deb91e9c3156ed46448c27f7f68d5
Sayantanmukh <dadai1913@gmail.com>
2022-03-25 15:02:20 +0530

    sp-boot

---
+++ src/main/java/com/capgemini/springboot/demo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.capgemini.springboot.demo.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.springboot.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
