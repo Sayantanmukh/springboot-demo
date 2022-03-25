f71a31b9a5b0572b25f267b2abe9eaf328da4600
Sayantanmukh <dadai1913@gmail.com>
2022-03-25 09:53:57 +0530

    requestbody

--- src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.springboot.demo.model.Employee;
@@ -37,19 +38,19 @@ public class EmployeeController {
 	}
 
 	@PostMapping("/add-employee")
-	public Employee addEmployee() {
-		Employee employee = new Employee();
-		LOG.info("Employee added successfully.");
+	public Employee addEmployee(@RequestBody Employee employee ) {
+		//Employee employee = new Employee();
+		LOG.info(employee.toString());
 		return service.addEmp(employee);
 	}
 
-//	public Employee updateEmployee() {
-//	return null;
-//}
-
-//	public Employee deleteEmployee() {
-//	return null;
-//}
+	public Employee updateEmployee() {
+		return null;
+	}
+	
+	public Employee deleteEmployee() {
+		return null;
+	}
 
 }
 
